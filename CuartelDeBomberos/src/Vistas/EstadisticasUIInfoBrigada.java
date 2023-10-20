f57fe20fa638e2656572ffef7c81a04ee21ce45b
LidonniGit <139288437+LidonniGit@users.noreply.github.com>
2023-10-20 18:43:28 -0300

    Descarto brigadas inactivas

--- CuartelDeBomberos/src/Vistas/EstadisticasUIInfoBrigada.java
+++ CuartelDeBomberos/src/Vistas/EstadisticasUIInfoBrigada.java
@@ -64,7 +64,7 @@ public class EstadisticasUIInfoBrigada extends javax.swing.JInternalFrame {
             Emergencia emergencia = new Emergencia();
             EmergenciaData emergenciaData = new EmergenciaData();
             ArrayList<Brigada> listaBrigadas = new ArrayList();
-            listaBrigadas = brigadaData.listarBrigadasSegunDisponibilidad(estado);
+            listaBrigadas = brigadaData.brigadasPorEstadoYDisponibilidad(true,estado);
             Cuartel cuartel = new Cuartel();
             CuartelData cuartelData = new CuartelData();
             for (Brigada brigada : listaBrigadas) {
@@ -77,7 +77,7 @@ public class EstadisticasUIInfoBrigada extends javax.swing.JInternalFrame {
             Emergencia emergencia = new Emergencia();
             EmergenciaData emergenciaData = new EmergenciaData();
             ArrayList<Brigada> listaBrigadas = new ArrayList();
-            listaBrigadas = brigadaData.listarBrigadasSegunDisponibilidad(estado);
+            listaBrigadas = brigadaData.brigadasPorEstadoYDisponibilidad(true,estado);
             Cuartel cuartel = new Cuartel();
             CuartelData cuartelData = new CuartelData();
             for (Brigada brigada : listaBrigadas) {
