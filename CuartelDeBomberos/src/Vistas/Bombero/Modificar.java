cf133a6aacbd809a56743f599fb82a08284ed107
Maribel Otamendi <maribel.otamendi@medtronic.com>
2023-10-27 20:39:55 -0300

    se fija el tamaño de las ventanas

--- CuartelDeBomberos/src/Vistas/Bombero/Modificar.java
+++ CuartelDeBomberos/src/Vistas/Bombero/Modificar.java
@@ -106,6 +106,8 @@ public class Modificar extends javax.swing.JDialog {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Modificación de Bombero");
+        setPreferredSize(new java.awt.Dimension(800, 600));
+        setResizable(false);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setText("DNI");
